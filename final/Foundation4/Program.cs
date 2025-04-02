3f37e2f39f9db55f2ae146b2909e527161838b91
Parkercecil <parkerbcecil@gmail.com>
2025-04-02 11:38:04 -0600

    1st code run

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,84 @@
 using System;
+using System.Collections.Generic;
+
+abstract class Activity
+{
+    protected DateTime Date;
+    protected int Minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        Date = date;
+        Minutes = minutes;
+    }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{Date:dd MMM yyyy} ({Minutes} min) - Distance: {GetDistance():F2} km, Speed: {GetSpeed():F2} kph, Pace: {GetPace():F2} min per km";
+    }
+}
+
+class Running : Activity
+{
+    private double Distance;
+
+    public Running(DateTime date, int minutes, double distance) : base(date, minutes)
+    {
+        Distance = distance;
+    }
+
+    public override double GetDistance() => Distance;
+    public override double GetSpeed() => (Distance / Minutes) * 60;
+    public override double GetPace() => Minutes / Distance;
+}
+
+class Cycling : Activity
+{
+    private double Speed;
+
+    public Cycling(DateTime date, int minutes, double speed) : base(date, minutes)
+    {
+        Speed = speed;
+    }
+
+    public override double GetDistance() => (Speed * Minutes) / 60;
+    public override double GetSpeed() => Speed;
+    public override double GetPace() => 60 / Speed;
+}
+
+class Swimming : Activity
+{
+    private int Laps;
+    private const double LapLengthKm = 50.0 / 1000;
+
+    public Swimming(DateTime date, int minutes, int laps) : base(date, minutes)
+    {
+        Laps = laps;
+    }
+
+    public override double GetDistance() => Laps * LapLengthKm;
+    public override double GetSpeed() => (GetDistance() / Minutes) * 60;
+    public override double GetPace() => Minutes / GetDistance();
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2022, 11, 3), 30, 4.8),
+            new Cycling(new DateTime(2022, 11, 3), 30, 20.0),
+            new Swimming(new DateTime(2022, 11, 3), 30, 20)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
